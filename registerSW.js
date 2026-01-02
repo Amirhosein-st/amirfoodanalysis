5907c5ca8ecebf8745647ec6cf54847e53f4e3c8
Amirhosein-st <amirhoseinsafari41@gmail.com>
2026-01-02 22:44:25 +0330

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/amirfoodanalysis/sw.js', { scope: '/amirfoodanalysis/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
